44239c2595a207b2ffa8a7008c4e7d9269f09373
Mirza Andira Nugraha <114473359+mirzaandira@users.noreply.github.com>
2024-03-07 23:00:20 +0700

    Persegi.java

---
+++ Persegi.java
@@ -0,0 +1,12 @@
+public class Persegi extends BangunDatar {
+    private double sisi;
+
+    public Persegi(double sisi) {
+        this.sisi = sisi;
+    }
+
+    @Override
+    public double hitungLuas() {
+        return sisi * sisi;
+    }
+}
