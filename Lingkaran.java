4ffa9eca7c8660b0be1d2d24e120abf14376a6b1
Mirza Andira Nugraha <114473359+mirzaandira@users.noreply.github.com>
2024-03-07 23:01:11 +0700

    Lingkaran.java

---
+++ Lingkaran.java
@@ -0,0 +1,12 @@
+public class Lingkaran extends BangunDatar {
+    private double jariJari;
+
+    public Lingkaran(double jariJari) {
+        this.jariJari = jariJari;
+    }
+
+    @Override
+    public double hitungLuas() {
+        return Math.PI * jariJari * jariJari;
+    }
+}
