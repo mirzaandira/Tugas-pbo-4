1b69388d86f829909132a498704b863b9f14919f
Mirza Andira Nugraha <114473359+mirzaandira@users.noreply.github.com>
2024-03-07 22:57:22 +0700

    Segitiga.java

---
+++ Segitiga.java
@@ -0,0 +1,9 @@
+public abstract class Segitiga extends BangunDatar {
+    protected double a, b, c;
+
+    public Segitiga(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
