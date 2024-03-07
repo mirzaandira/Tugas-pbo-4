2fcd7851232564daa362091d875a0921095f0463
Mirza Andira Nugraha <114473359+mirzaandira@users.noreply.github.com>
2024-03-07 23:02:10 +0700

    App.java

--- App.java
+++ App.java
@@ -1,3 +1,11 @@
-public abstract class BangunDatar {
-    public abstract double hitungLuas();
+public class App {
+    public static void main(String[] args) {
+        Persegi persegi = new Persegi(5);
+        Segitiga segitiga = new SegitigaSikuSiku(3, 4);
+        Lingkaran lingkaran = new Lingkaran(2);
+
+        System.out.println("Luas Persegi: " + persegi.hitungLuas());
+        System.out.println("Luas Segitiga: " + segitiga.hitungLuas());
+        System.out.println("Luas Lingkaran: " + lingkaran.hitungLuas());
+    }
 }
